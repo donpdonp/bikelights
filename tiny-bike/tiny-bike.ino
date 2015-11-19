79590a1dc758307c80eb8e6c361138792efafc3f
Don Park <donp@donp.org>
2015-11-18 19:06:53 -0800

    some randomness

--- tiny-bike/tiny-bike.ino
+++ tiny-bike/tiny-bike.ino
@@ -1,3 +1,4 @@
+#define USE_HSV 1
 #include <WS2812.h>
 
 WS2812 NeoA(16); 
@@ -9,7 +10,8 @@ int ledR = 3;
 int ledG = 4;
 
 void setup() {
-  // put your setup code here, to run once:
+  randomSeed(analogRead(0));
+
   NeoA.setOutput(2); 
   //NeoB.setOutput(5); 
   pinMode(ledG, OUTPUT);
@@ -17,12 +19,14 @@ void setup() {
 
   digitalWrite(ledG, HIGH);
 
-  color.r = 255; color.g = 255; color.b = 255;
+  // 0-7 front lights
+  color.r = 155; color.g = 155; color.b = 155;
   for(int i=0; i < 8; i++) {
     NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
   }
 
-  color.r = 255; color.g = 0; color.b = 0; // RGB color -> Red
+  // 8-15 back lights
+  color.r = 155; color.g = 0; color.b = 0;
   for(int i=8; i < 16; i++) {
     NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
   }
@@ -32,51 +36,52 @@ void setup() {
 
 void loop() {
   
-  digitalWrite(ledR, HIGH);
-    
-  for(int i=8; i < 16; i++) {
-    color.r = 0; color.g = 0; color.b = 0; // RGB color -> Half Red
+  // cycle front strip
+  digitalWrite(ledR, HIGH);  
+  for(int i=0; i < 8; i++) {
+    color.r = 0; color.g = 150; color.b = 0; // RGB color -> Half Red
     NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
     NeoA.sync(); // Sends the color to the LED
     delay(90); 
-    color.r = 255; color.g = 0; color.b = 0; // RGB color -> Half Red
+    color.r = 155; color.g = 155; color.b = 155;
+    NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
+    NeoA.sync(); // Sends the color to the LED
+    delay(90); 
+  }
+
+  for(int i=7; i >= 0; i--) {
+    color.r = 0; color.g = 0; color.b = 155;
+    NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
+    NeoA.sync(); // Sends the color to the LED
+    delay(90); 
+    color.r = 255; color.g = 255; color.b = 255;
+    NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
+    NeoA.sync(); // Sends the color to the LED
+    delay(90); 
+  }
+
+  delay(300); 
+
+
+  // cycle back strip
+  digitalWrite(ledR, LOW); 
+  for(int i=8; i < 16; i++) {
+    color.SetHSV(random(255),255,155);
+    NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
+    NeoA.sync(); // Sends the color to the LED
+    delay(150); 
+    color.r = 155; color.g = 0; color.b = 0;
     NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
     NeoA.sync(); // Sends the color to the LED
     delay(90); 
   }
 
   for(int i=15; i > 7; i--) {
-    color.r = 0; color.g = 0; color.b = 0; // RGB color -> Half Red
+    color.SetHSV(random(255),255,155);
     NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
     NeoA.sync(); // Sends the color to the LED
-    delay(90); 
-    color.r = 255; color.g = 0; color.b = 0; // RGB color -> Half Red
-    NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
-    NeoA.sync(); // Sends the color to the LED
-    delay(90); 
-  }
-
-  delay(300); // Wait 500 ms
-
-  digitalWrite(ledR, LOW); 
-  
-  for(int i=0; i < 8; i++) {
-    color.r = 0; color.g = 255; color.b = 0; // RGB color -> Half Red
-    NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
-    NeoA.sync(); // Sends the color to the LED
-    delay(50); 
-    color.r = 255; color.g = 255; color.b = 255; // RGB color -> Half Red
-    NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
-    NeoA.sync(); // Sends the color to the LED
-    delay(50); 
-  }
-
-  for(int i=7; i >= 0; i--) {
-    color.r = 0; color.g = 0; color.b = 255; // RGB color -> Half Red
-    NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
-    NeoA.sync(); // Sends the color to the LED
-    delay(90); 
-    color.r = 255; color.g = 255; color.b = 255; // RGB color -> Half Red
+    delay(150); 
+    color.r = 155; color.g = 0; color.b = 0;
     NeoA.set_crgb_at(i, color); // Set color at LED found at index 0
     NeoA.sync(); // Sends the color to the LED
     delay(90); 
